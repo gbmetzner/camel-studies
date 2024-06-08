16cdc0e92a415507e6e030dc87e6e9ae040a5229
Gustavo Belloni Metzner <gbellonimetzner@central1.com>
2024-06-08 09:04:50 -0700

    adding rabbitmq

---
+++ rabbitmq-camel/src/main/java/com/gbm/rabbitmqcamel/RabbitmqCamelApplication.java
@@ -0,0 +1,13 @@
+package com.gbm.rabbitmqcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqCamelApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-camel/src/test/java/com/gbm/rabbitmqcamel/RabbitmqCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbm.rabbitmqcamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
